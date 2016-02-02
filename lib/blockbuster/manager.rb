370ed1538ad0ba40f65f57394dcf2a813398fbd1
Lukas Eklund <leklund@fastly.com>
2016-02-01 21:42:54 -0600

    add cassette md5 hash comparison methods

--- lib/blockbuster/manager.rb
+++ lib/blockbuster/manager.rb
@@ -6,9 +6,8 @@ module Blockbuster
     TEST_DIRECTORY     = 'test'.freeze
 
     attr_reader :cassette_directory, :cassette_file, :test_directory, :silent
+    attr_accessor :comparison_hash
 
-    # creates a new manager
-    #
     # @param cassette_directory [String] Name of directory cassette files are stored.
     #  Will be stored under the test directory. default: 'casssettes'
     # @param cassette_file [String] name of gz cassettes file. default: 'vcr_cassettes.tar.gz'
@@ -20,28 +19,70 @@ module Blockbuster
       @cassette_file      = cassette_file
       @test_directory     = test_directory
       @silent             = silent
+      @comparison_hash    = {}
     end
 
+    # extracts cassettes from a tar.gz file
+    #
+    # tracks a md5 hash of each file in the tarball
     def rent
-      return false if ENV['VCR_MODE'] == 'local'
       unless File.exist?(cassette_file_path)
-        silent_puts "File does not exist #{cassette_file_path}"
+        silent_puts "[Blockbuster] File does not exist #{cassette_file_path}."
         return false
       end
       # return unless File.exist?(cassette_file_path)
 
-      silent_puts "Extracting VCR cassettes to #{cassette_dir}"
+      silent_puts "[Blockbuster] Extracting VCR cassettes to #{cassette_dir}"
 
       extract_cassettes
     end
 
+    # repackages cassettes into a compressed tarball
     def return
+      if rewind?
+        silent_puts puts "Recreating cassette file #{CASSETTE_FILE}"
+        create_cassette_file if rewind
+      end
+    end
+
+    # performs comparison of files
+    #
+    # compares the md5 sums of the files in the existing tarball
+    # and the cassettes from the cassette directory
+    def rewind?
+      files = Dir.glob("#{cassette_dir}/**/*")
+      retval = true if files.size != comparison_hash.keys.size
+      files.each do |file|
+        key = key_from_path(file)
+        comp = compare_cassettes(key, file)
+        retval ||= comp
+      end
+      retval.nil? ? false : retval
+    end
+
+    # returns true for any differences or changes in cassette files
+    def compare_cassettes(key, file)
+      orig_key = comparison_hash.delete(key)
+      if orig_key.nil?
+        silent_puts "New cassette: #{key}"
+        return true
+      elsif orig_key != file_digest(file)
+        silent_puts "Cassette changed: #{key}"
+        return true
+      end
     end
 
     alias setup rent
     alias teardown return
+    alias compare rewind?
 
     private
+
+    def key_from_path(file)
+      path_array = File.dirname(file).split('/')
+      idx = path_array.index(cassette_directory)
+      path_array[idx..-1].push(File.basename(file)).join('/')
+    end
 
     def cassette_dir
       File.join(test_directory, cassette_directory)
@@ -49,6 +90,10 @@ module Blockbuster
 
     def cassette_file_path
       File.join(test_directory, cassette_file)
+    end
+
+    def create_cassette_file
+
     end
 
     def extract_cassettes
@@ -64,13 +109,15 @@ module Blockbuster
       end
     end
 
-    def write_file(entry)
-      destination = File.join test_directory, entry.full_name
-      FileUtils.mkdir_p(File.dirname(destination))
-      File.open(destination, 'wb') do |cass|
-        cass.write(entry.read)
-      end
-      File.chmod(entry.header.mode, destination)
+    def file_digest(file)
+      Digest::MD5.file(file).hexdigest
+    end
+
+    def read_entry_and_hash(entry)
+      contents = entry.read
+
+      comparison_hash[entry.full_name] = Digest::MD5.hexdigest(contents)
+      contents
     end
 
     def silent?
@@ -78,59 +125,21 @@ module Blockbuster
     end
 
     def silent_puts(msg)
-      puts msg unless silent?
+      puts "[Blockbuster] #{msg}" unless silent?
+    end
+
+    def write_file(entry)
+      destination = File.join test_directory, entry.full_name
+
+      contents = read_entry_and_hash(entry)
+
+      return if ENV['VCR_MODE'] == 'local'
+
+      FileUtils.mkdir_p(File.dirname(destination))
+      File.open(destination, 'wb') do |cass|
+        cass.write(contents)
+      end
+      File.chmod(entry.header.mode, destination)
     end
   end
 end
-
-=begin
-  def setup
-    return if ENV['VCR_MODE'] == 'local'
-    unless File.exist?(cassette_file_path)
-      puts "File does not exist #{cassette_file_path}"
-      puts "PWD #{FileUtils.pwd}"
-      return
-    end
-    # return unless File.exist?(cassette_file_path)
-
-    puts "Extracting VCR cassettes to #{cassette_dir}"
-    status = system("tar -xzvf #{cassette_file_path} -C #{base}/ > /dev/null")
-    puts 'WARN: could not extract cassettes' unless status
-  end
-
-  def teardown
-    if File.exist?(cassette_file_path)
-      compare_cassettes
-    else
-      create_cassette_file
-    end
-  end
-
-  private
-
-
-  def compare_cassettes
-    Dir.mkdir(tmpdir) unless Dir.exist?(tmpdir)
-
-    `tar -xzf #{cassette_file_path} -C #{tmpdir}`
-    status = system("diff -arq  #{cassette_dir} #{tmpdir}/#{CASSETTE_DIR_NAME}")
-
-    unless status
-      puts "Cassettes are different. Recreating cassette file #{CASSETTE_FILE}"
-      create_cassette_file
-    end
-
-    FileUtils.remove_dir(tmpdir)
-  end
-
-  def create_cassette_file
-    `cd #{base} && tar -czvf #{CASSETTE_FILE} #{CASSETTE_DIR_NAME}`
-  end
-
-  def tmpdir
-    "#{base}/tmp"
-  end
-end
-
-=end
-
--- spec/blockbuster_spec.rb
+++ spec/blockbuster_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'spec_helper'
 
 describe Blockbuster do
@@ -35,45 +36,103 @@ describe Blockbuster do
         mgr = klass.new(test_directory: tst_dir)
         mgr.test_directory.must_equal tst_dir
       end
+
+      it 'initializes with an empty comparison_hash' do
+        manager = klass.new
+        manager.comparison_hash.must_equal({})
+      end
     end
 
-    describe '.rent' do
-      let(:manager)      { klass.new }
-      let(:my_test_dir)  { File.join(File.dirname(__FILE__), 'fixtures') }
-      let(:cassette_dir) { File.join(my_test_dir, 'cassettes') }
-      let(:cassette_1)   { File.join(cassette_dir, 'match_requests_on.yml') }
-      let(:cassette_2)   { File.join(cassette_dir, 'fake_example_response.yml') }
+    describe 'instance methods' do
+      let(:my_test_dir)       { File.join(File.dirname(__FILE__), 'fixtures') }
+      let(:cassette_dir)      { 'cassettes' }
+      let(:cassette_dir_path) { File.join(my_test_dir, 'cassettes') }
+      let(:cassette_1)        { File.join(cassette_dir_path, 'match_requests_on.yml') }
+      let(:cassette_2)        { File.join(cassette_dir_path, 'fake_example_response.yml') }
+      let(:manager) do
+        klass.new(test_directory: my_test_dir,
+                  cassette_file: 'test_cassettes.tar.gz',
+                  cassette_directory: cassette_dir,
+                  silent: true)
+      end
 
       after do
         ENV['VCR_MODE'] = nil
         FileUtils.rm_r(cassette_dir) if Dir.exist?(cassette_dir)
       end
 
-      it 'does nothing if ENV["VCR_MODE"] equals "local"' do
-        ENV['VCR_MODE'] = 'local'
-        manager.rent.must_equal false
+      describe '.rent' do
+        it 'does nothing if ENV["VCR_MODE"] equals "local"' do
+          ENV['VCR_MODE'] = 'local'
+          manager.rent
+
+          Dir.exist?(cassette_dir).must_equal false
+        end
+
+        it 'returns false if the cassette file does not exists' do
+          manager = klass.new(cassette_file: 'nosuchfile.tar.gz', silent: true)
+
+          manager.rent.must_equal false
+        end
+
+        it 'extracts cassette files' do
+          manager.cassette_file.must_equal 'test_cassettes.tar.gz'
+          manager.rent
+
+          File.exist?(cassette_1).must_equal true
+          File.exist?(cassette_2).must_equal true
+          FileUtils.identical?(cassette_1, File.join(my_test_dir, 'match_requests_on.yml')).must_equal true
+          FileUtils.identical?(cassette_2, File.join(my_test_dir, 'fake_example_response.yml')).must_equal true
+        end
+
+        it 'has an alias' do
+          manager.must_respond_to :setup
+        end
+
+        it 'tracks a hash of each file for comparison upon teardown' do
+          manager.rent
+
+          manager.comparison_hash.keys.must_include 'cassettes/match_requests_on.yml'
+          manager.comparison_hash.keys.must_include 'cassettes/fake_example_response.yml'
+        end
       end
 
-      it 'returns false if the cassette file does not exists' do
-        manager = klass.new(cassette_file: 'nosuchfile.tar.gz', silent: true)
+      describe '.return' do
 
-        manager.rent.must_equal false
       end
 
-      it 'extracts cassette files' do
-        manager = klass.new(test_directory: my_test_dir, cassette_file: 'test_cassettes.tar.gz', cassette_directory: cassette_dir, silent: true)
+      describe '.rewind?' do
+        before do
+          manager.rent
+        end
 
-        manager.cassette_file.must_equal 'test_cassettes.tar.gz'
-        manager.rent
+        it 'returns false if no files have changed' do
+          manager.rewind?.must_equal false
+        end
 
-        File.exist?(cassette_1).must_equal true
-        File.exist?(cassette_2).must_equal true
-        FileUtils.identical?(cassette_1, File.join(my_test_dir, 'match_requests_on.yml')).must_equal true
-        FileUtils.identical?(cassette_2, File.join(my_test_dir, 'fake_example_response.yml')).must_equal true
-      end
+        it 'returns true if a cassette file was changed' do
+          open(cassette_2, 'a') { |f|
+            f << 'new recording'
+          }
+          manager.rewind?.must_equal true
+        end
 
-      it 'has an alias' do
-        manager.must_respond_to :setup
+        it 'returns true if no comparison_hash was created' do
+          manager.comparison_hash = {}
+          manager.rewind?.must_equal true
+        end
+
+        it 'returns false if a file was deleted from the cassettes directory' do
+          FileUtils.rm(cassette_1)
+          manager.rewind?.must_equal true
+        end
+
+        it 'returns false if a file was added to the cassettes directory' do
+          new_cass = File.join(cassette_dir_path, 'new_cass.yml')
+          FileUtils.touch(new_cass)
+          manager.rewind?.must_equal true
+          FileUtils.rm(new_cass)
+        end
       end
     end
   end
