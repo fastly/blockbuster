b821a56cc0b7bf41a9f006df40c67422ecc18380
Hassan Shahid <hassan@fastly.com>
2016-04-01 10:08:32 -0400

    add compare method to comparator (moved from manager)

--- lib/blockbuster/comparator.rb
+++ lib/blockbuster/comparator.rb
@@ -1,6 +1,8 @@
 module Blockbuster
   # Object that holds all cassettes and their latest checksum
   class Comparator
+    include Blockbuster::OutputHelpers
+
     def initialize
       @hash = {}
     end
@@ -19,6 +21,19 @@ module Blockbuster
 
     def present?
       !@hash.keys.empty?
+    end
+
+    # returns true for any differences or changes in cassette files
+    def compare(key, new_digest)
+      digest = @hash.delete(key)
+
+      if digest.nil?
+        silent_puts "New cassette: #{key}"
+        return true
+      elsif digest != new_digest
+        silent_puts "Cassette changed: #{key}"
+        return true
+      end
     end
   end
 end
