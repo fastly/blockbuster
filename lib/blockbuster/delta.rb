5364d21fd0517323c1a60fd20618cf552c7821b7
Hassan Shahid <hassan@fastly.com>
2016-04-04 02:35:41 -0400

    Delta class work

--- lib/blockbuster/delta.rb
+++ lib/blockbuster/delta.rb
@@ -1,6 +1,11 @@
 module Blockbuster
   # Delta file objects
   class Delta
+    include Blockbuster::Extractor
+    include Blockbuster::Packager
+    include Blockbuster::FileHelpers
+    extend Blockbuster::FileHelpers
+
     # nodoc
     class NotEnabledError < StandardError
       def message
@@ -19,21 +24,45 @@ module Blockbuster
     end
 
     def self.initialize_for_each
-      files.map do |file|
+      setup_directory
+
+      delta_files = files
+
+      if delta_files.empty?
+        delta_files = ["#{full_delta_directory}/#{Blockbuster.configuration.current_delta_name}"]
+      end
+
+      delta_files.map do |file|
         new(file)
       end
     end
 
-    attr_reader :current
+    def self.setup_directory
+      return if Dir.exist?(full_delta_directory)
+
+      FileUtils.mkdir_p(full_delta_directory)
+      FileUtils.touch("#{full_delta_directory}/.keep")
+    end
+
+    attr_reader :current, :file_name
 
     def initialize(file_name)
       raise NotEnabledError unless Blockbuster.configuration.deltas_enabled?
 
-      @current = true if File.basename(file_name) == Blockbuster.configuration.current_delta_name
+      @file_name = file_name
+      @current   = true if File.basename(file_name) == Blockbuster.configuration.current_delta_name
     end
 
     def current
       @current || false
+    end
+
+    def compare?
+      !current?
+    end
+
+    def file_path
+      File.join(full_delta_directory, file_name)
     end
 
     alias current? current
