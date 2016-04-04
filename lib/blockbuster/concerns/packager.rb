879dbc4428893d6dd11a6b3322735f1cfbc9a3bf
Hassan Shahid <hassan@fastly.com>
2016-04-04 02:33:19 -0400

    Packager concern work

--- lib/blockbuster/concerns/packager.rb
+++ lib/blockbuster/concerns/packager.rb
@@ -1,17 +1,38 @@
 module Blockbuster
   # pure ruby implmentation of tar gzip and diff
   module Packager
-    private
-
     def create_cassette_file
-      FileUtils.rm(master_tar_file_path) if File.exist?(master_tar_file_path)
-      File.open(master_tar_file_path, 'wb') do |file|
+      FileUtils.rm(file_path) if File.exist?(file_path)
+      File.open(file_path, 'wb') do |file|
         Zlib::GzipWriter.wrap(file) do |gz|
-          create_tar(gz) do |tar|
-            Dir.glob(File.join(cassette_dir, '**/*')).each do |cass|
+          Gem::Package::TarWriter.new(gz) do |tar|
+            cassette_files.each do |cass|
               tar_file(tar, cass)
             end
           end
+        end
+      end
+    end
+
+    def tar_file(tar, file)
+      rel_path = key_from_path(file)
+
+      if Blockbuster.configuration.deltas_enabled?
+        return unless Blockbuster.comparator.edited?(rel_path)
+      end
+
+      write_to_disk(tar, file)
+    end
+
+    def write_to_disk(tar, file)
+      mode     = File.stat(file).mode
+      rel_path = key_from_path(file)
+
+      if File.directory?(file)
+        tar.mkdir rel_path, mode
+      else
+        tar.add_file_simple rel_path, mode, File.size(file) do |io|
+          File.open(file, 'rb') { |f| io.write f.read }
         end
       end
     end
