17c56aa6c8262cfc6d25e119857727e0383f2ffd
Hassan Shahid <hassan@fastly.com>
2016-04-04 02:37:05 -0400

    Comparator work

--- lib/blockbuster/comparator.rb
+++ lib/blockbuster/comparator.rb
@@ -1,14 +1,18 @@
 module Blockbuster
   # Object that holds all cassettes and their latest checksum
   class Comparator
+    include Blockbuster::FileHelpers
     include Blockbuster::OutputHelpers
 
     def initialize
       @hash = {}
+
+      @edited              = []
+      @current_delta_files = []
     end
 
-    def add(key, value)
-      @hash[key] = value
+    def add(key, value, source)
+      @hash[key] = { 'content' => value, 'source' => source }
     end
 
     def delete(key)
@@ -30,10 +34,37 @@ module Blockbuster
       if digest.nil?
         silent_puts "New cassette: #{key}"
         return true
-      elsif digest != new_digest
+      elsif digest['content'] != new_digest
         silent_puts "Cassette changed: #{key}"
         return true
       end
+    end
+
+    def edited?(file)
+      @edited.include?(file)
+    end
+
+    def store_current_delta_files
+      @hash.each do |k, v|
+        @current_delta_files << k if v['source'] == Blockbuster.configuration.current_delta_name
+      end
+    end
+
+    # compares the md5 sums of the files in the existing tarball
+    # and the cassettes from the cassette directory.
+    def rewind?(files)
+      files.each do |file|
+        next unless File.file?(file)
+        key = key_from_path(file)
+        @edited << key if compare(key, file_digest(file))
+      end
+
+      if present? && Blockbuster.configuration.deltas_disabled?
+        silent_puts "Cassettes deleted: #{keys}"
+        return true
+      end
+
+      !@edited.empty?
     end
   end
 end
--- spec/blockbuster/comparator_spec.rb
+++ spec/blockbuster/comparator_spec.rb
@@ -7,14 +7,14 @@ describe Blockbuster::Comparator do
   describe '#add' do
     it 'adds the given key and value to the comparison object' do
       instance.keys.wont_include 'a'
-      instance.add('a', 'b')
+      instance.add('a', 'b', 'c')
       instance.keys.must_include 'a'
     end
   end
 
   describe '#delete' do
     it 'deletes the given key' do
-      instance.add('a', 'b')
+      instance.add('a', 'b', 'c')
       instance.keys.must_include 'a'
       instance.delete('a')
       instance.keys.wont_include 'a'
@@ -33,8 +33,8 @@ describe Blockbuster::Comparator do
     end
 
     it 'returns an array of the comparison keys' do
-      instance.add('a', 'b')
-      instance.add('b', 'c')
+      instance.add('a', 'b', 'c')
+      instance.add('b', 'c', 'd')
       instance.keys.size.must_be :>, 0
       instance.keys.sort.must_equal %w(a b)
     end
@@ -47,9 +47,90 @@ describe Blockbuster::Comparator do
     end
 
     it 'returns true when there are keys' do
-      instance.add('a', 'b')
+      instance.add('a', 'b', 'c')
       instance.keys.wont_be_empty
       instance.present?.must_equal true
+    end
+  end
+
+  describe '#compare' do
+    it 'returns true if first argument does not exist as hash key' do
+      instance.keys.include?('fakekey').must_equal false
+
+      instance.compare('fakekey', 'blah').must_equal true
+    end
+
+    it 'returns true if hash-key "content" value != second argument' do
+      instance.add('somekey', 'somevalue', 'somesource')
+
+      instance.compare('somekey', 'newvalue').must_equal true
+    end
+
+    it 'returns nil otherwise' do
+      instance.add('somekey', 'somevalue', 'somesource')
+
+      instance.compare('somekey', 'somevalue').must_equal nil
+    end
+  end
+
+  describe '#edited?' do
+    it 'returns false if given argument does not exist in internal edited array' do
+      instance.instance_variable_get(:@edited).must_equal []
+
+      instance.edited?('somename').must_equal false
+    end
+
+    it 'returns true if given argument exists in internal edited array' do
+      instance.instance_variable_set(:@edited, ['i_exist'])
+
+      instance.edited?('i_exist').must_equal true
+    end
+  end
+
+  describe '#store_current_delta_files' do
+    it 'does not add files to current_delta_files list if `source` != `current_delta_name`' do
+      source_name = 'blah'
+      source_name.wont_equal Blockbuster.configuration.current_delta_name
+
+      instance.add('a', 'b', source_name)
+
+      instance.store_current_delta_files
+
+      instance.instance_variable_get(:@current_delta_files).wont_include 'a'
+    end
+
+    it 'adds files to current_delta_files list if `source` == `current_delta_name' do
+      instance.add('a', 'b', Blockbuster.configuration.current_delta_name)
+
+      instance.store_current_delta_files
+
+      instance.instance_variable_get(:@current_delta_files).must_include 'a'
+    end
+  end
+
+  describe '#rewind?' do
+    it 'returns true if there are keys present and deltas are disabled' do
+      instance.add('a', 'b', 'c')
+      instance.present?.must_equal true
+      Blockbuster.configuration.deltas_disabled?.must_equal true
+
+      instance.rewind?([]).must_equal true
+    end
+
+    it 'returns false if there are no edited files' do
+      instance.present?.must_equal false
+
+      instance.instance_variable_get(:@edited).must_be :empty?
+
+      instance.rewind?([]).must_equal false
+    end
+
+    it 'returns true if there are edited files' do
+      instance.present?.must_equal false
+
+      instance.instance_variable_set(:@edited, ['a'])
+
+      instance.rewind?([]).must_equal true
     end
   end
 end
