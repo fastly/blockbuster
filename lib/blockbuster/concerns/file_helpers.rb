05b71d7ac6f882f0dcc01cc0706ccff6c39f3083
Hassan Shahid <hassan@fastly.com>
2016-04-04 02:29:35 -0400

    Add more helpers to file_helpers concern.
    also, make the concerns self-sufficient by looking at the configuration
    singleton, rather than assume it exists on a class the module will be
    included in.

--- lib/blockbuster/concerns/file_helpers.rb
+++ lib/blockbuster/concerns/file_helpers.rb
@@ -3,7 +3,7 @@ module Blockbuster
   module FileHelpers
     def key_from_path(file)
       path_array = File.dirname(file).split('/')
-      idx = path_array.index(cassette_directory)
+      idx = path_array.index(Blockbuster.configuration.cassette_directory)
       path_array[idx..-1].push(File.basename(file)).join('/')
     end
 
@@ -11,12 +11,24 @@ module Blockbuster
       Digest::MD5.file(file).hexdigest
     end
 
+    def tar_digest(content)
+      Digest::MD5.hexdigest(content)
+    end
+
     def cassette_dir
-      File.join(test_directory, cassette_directory)
+      File.join(Blockbuster.configuration.test_directory, Blockbuster.configuration.cassette_directory)
     end
 
     def master_tar_file_path
-      File.join(test_directory, master_tar_file)
+      File.join(Blockbuster.configuration.test_directory, Blockbuster.configuration.master_tar_file)
+    end
+
+    def full_delta_directory
+      File.join(Blockbuster.configuration.test_directory, Blockbuster.configuration.delta_directory)
+    end
+
+    def cassette_files
+      Dir.glob("#{cassette_dir}/**/*")
     end
   end
 end
