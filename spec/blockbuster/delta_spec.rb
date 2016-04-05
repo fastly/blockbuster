4bba15b4344733f463eb5507431b03e6e930f870
pabergman <alexander@fastly.com>
2016-04-05 12:24:28 +0100

    Finish delta specs.

--- spec/blockbuster/delta_spec.rb
+++ spec/blockbuster/delta_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Blockbuster::Delta do
-  let(:klass) { Blockbuster::Delta }
-  let(:dir)   { klass.full_delta_directory }
+  let(:klass)              { Blockbuster::Delta }
+  let(:current_delta_name) { Blockbuster.configuration.current_delta_name }
+  let(:dir)                { klass.full_delta_directory }
+  let(:current_time)       { Time.now.to_i }
 
   describe '.files' do
-
     it 'returns an empty array when there are no tests' do
       FileUtils.rm Dir.glob("#{klass.full_delta_directory}/*.tar.gz")
       klass.files.must_equal []
@@ -14,17 +15,18 @@ describe Blockbuster::Delta do
     it 'returns an list of files ordered by time' do
       dir = klass.full_delta_directory
       FileUtils.rm Dir.glob("#{dir}/*.tar.gz")
-      time = Time.now.to_i
-      FileUtils.touch("#{dir}/#{time - 30}_c.tar.gz")
-      FileUtils.touch("#{dir}/#{time - 20}_b.tar.gz")
-      FileUtils.touch("#{dir}/#{time - 10}_a.tar.gz")
 
-      klass.files.must_equal ["#{dir}/#{time - 30}_c.tar.gz", "#{dir}/#{time - 20}_b.tar.gz", "#{dir}/#{time - 10}_a.tar.gz"]      
+      FileUtils.touch("#{dir}/#{current_time - 30}_c.tar.gz")
+      FileUtils.touch("#{dir}/#{current_time - 20}_b.tar.gz")
+      FileUtils.touch("#{dir}/#{current_time - 10}_a.tar.gz")
+
+      klass.files.must_equal ["#{dir}/#{current_time - 30}_c.tar.gz",
+                              "#{dir}/#{current_time - 20}_b.tar.gz",
+                              "#{dir}/#{current_time - 10}_a.tar.gz"]
     end
   end
 
   describe '.initialize_for_each' do
-
     it 'calls .setup_directory and .files once' do
       FileUtils.rm Dir.glob("#{dir}/*.tar.gz")    
       klass.expects(:setup_directory)
@@ -48,12 +50,11 @@ describe Blockbuster::Delta do
       deltas = klass.initialize_for_each
       deltas.size.must_equal 1
       deltas[0].current?.must_equal true
-      deltas[0].file_path.must_equal "#{dir}/#{Blockbuster.configuration.current_delta_name}"
+      deltas[0].file_path.must_equal "#{dir}/#{current_delta_name}"
     end
   end
 
   describe '.setup_directory' do
-
     it 'returns early if the directory exists' do
       klass.setup_directory
       FileUtils.expects(:mkdir_p).never
@@ -65,8 +66,48 @@ describe Blockbuster::Delta do
       FileUtils.rm_r dir
 
       klass.setup_directory
-      File.directory?(dir)
+      File.directory?(dir).must_equal true
       File.exist?("#{dir}/.keep").must_equal true
+    end
+  end
+
+  let(:delta) { klass.new("#{dir}/#{current_time}_#{current_delta_name}") }
+
+  describe '#initialize' do
+    it 'raises NotEnabledError if deltas are not enabled' do
+      Blockbuster.configuration.stubs(:deltas_disabled?).returns(true)
+
+      proc { klass.new('') }.must_raise klass::NotEnabledError
+    end
+
+    it 'sets @current to true if it is the current delta using a fuzzy match' do
+      Blockbuster.configuration.stubs(:deltas_disabled?).returns(false)
+
+      delta.current?.must_equal true
+    end
+
+    it 'the filename does not include the path' do
+      Blockbuster.configuration.stubs(:deltas_disabled?).returns(false)
+
+      delta.file_name.must_equal "#{current_time}_#{current_delta_name}"
+    end
+  end
+
+  describe '#file_path' do
+    it 'is the delta director + filename' do
+    Blockbuster.configuration.stubs(:deltas_disabled?).returns(false)
+
+    delta.file_path.must_equal "#{dir}/#{current_time}_#{current_delta_name}"
+    end
+  end
+
+  describe '#target_path' do
+    it 'is the director + the delta name prefixed with the current time in epoch format' do
+    Blockbuster.configuration.stubs(:deltas_disabled?).returns(false)
+
+    current_time
+    Time.expects(:now).returns(current_time)
+    delta.target_path.must_equal "#{dir}/#{current_time}_#{current_delta_name}"
     end
   end
 end
