4690e3fd69bb2b2fa5338c1fd3f000f7773442ff
Hassan Shahid <hassan@fastly.com>
2016-04-08 01:52:35 -0400

    Stick to describe block, remove module in test.
    Primarily for rubocop.

--- spec/blockbuster/configuration_spec.rb
+++ spec/blockbuster/configuration_spec.rb
@@ -1,164 +1,162 @@
 require 'spec_helper'
 
-module Blockbuster
-  describe Configuration do
-    let(:klass)         { Configuration }
-    let(:configuration) { klass.new }
+describe Blockbuster::Configuration do
+  let(:klass)         { Blockbuster::Configuration }
+  let(:configuration) { klass.new }
 
-    it 'is an instance of Blockbuster::Configuration' do
-      configuration.must_be_instance_of klass
+  it 'is an instance of Blockbuster::Configuration' do
+    configuration.must_be_instance_of klass
+  end
+
+  it 'has configuration attributes' do
+    attrs = [
+      :cassette_directory,
+      :master_tar_file,
+      :local_mode,
+      :test_directory,
+      :wipe_cassette_dir,
+      :silent,
+      :enable_deltas,
+      :delta_directory,
+      :current_delta_name
+    ]
+
+    attrs.each do |attr|
+      assert_respond_to configuration, attr
+    end
+  end
+
+  describe '#cassette_directory' do
+    it 'defaults to CASSETTE_DIRECTORY' do
+      configuration.cassette_directory.must_equal klass::CASSETTE_DIRECTORY
     end
 
-    it 'has configuration attributes' do
-      attrs = [
-        :cassette_directory,
-        :master_tar_file,
-        :local_mode,
-        :test_directory,
-        :wipe_cassette_dir,
-        :silent,
-        :enable_deltas,
-        :delta_directory,
-        :current_delta_name
-      ]
+    it 'returns assigned value' do
+      configuration.cassette_directory = 'somedir'
 
-      attrs.each do |attr|
-        assert_respond_to configuration, attr
-      end
+      configuration.cassette_directory.must_equal 'somedir'
+    end
+  end
+
+  describe '#master_tar_file' do
+    it 'defaults to MASTER_TAR_FILE' do
+      configuration.master_tar_file.must_equal klass::MASTER_TAR_FILE
     end
 
-    describe '#cassette_directory' do
-      it 'defaults to CASSETTE_DIRECTORY' do
-        configuration.cassette_directory.must_equal klass::CASSETTE_DIRECTORY
-      end
+    it 'returns assigned value' do
+      configuration.master_tar_file = 'somefile'
 
-      it 'returns assigned value' do
-        configuration.cassette_directory = 'somedir'
+      configuration.master_tar_file.must_equal 'somefile'
+    end
+  end
 
-        configuration.cassette_directory.must_equal 'somedir'
-      end
+  describe '#test_directory' do
+    it 'defaults to TEST_DIRECTORY' do
+      configuration.test_directory.must_equal klass::TEST_DIRECTORY
     end
 
-    describe '#master_tar_file' do
-      it 'defaults to MASTER_TAR_FILE' do
-        configuration.master_tar_file.must_equal klass::MASTER_TAR_FILE
-      end
+    it 'returns assigned value' do
+      configuration.test_directory = 'somedir'
 
-      it 'returns assigned value' do
-        configuration.master_tar_file = 'somefile'
+      configuration.test_directory.must_equal 'somedir'
+    end
+  end
 
-        configuration.master_tar_file.must_equal 'somefile'
-      end
+  describe '#silent' do
+    it 'defaults to SILENT' do
+      configuration.silent.must_equal klass::SILENT
     end
 
-    describe '#test_directory' do
-      it 'defaults to TEST_DIRECTORY' do
-        configuration.test_directory.must_equal klass::TEST_DIRECTORY
-      end
+    it 'returns assigned value' do
+      configuration.silent = true
 
-      it 'returns assigned value' do
-        configuration.test_directory = 'somedir'
+      configuration.silent.must_equal true
+    end
+  end
 
-        configuration.test_directory.must_equal 'somedir'
-      end
+  describe '#wipe_cassette_dir' do
+    it 'defaults to WIPE_CASSETTE_DIR' do
+      configuration.wipe_cassette_dir.must_equal klass::WIPE_CASSETTE_DIR
     end
 
-    describe '#silent' do
-      it 'defaults to SILENT' do
-        configuration.silent.must_equal klass::SILENT
-      end
+    it 'returns assigned value' do
+      configuration.wipe_cassette_dir = true
 
-      it 'returns assigned value' do
-        configuration.silent = true
+      configuration.wipe_cassette_dir.must_equal true
+    end
+  end
 
-        configuration.silent.must_equal true
-      end
+  describe '#local_mode' do
+    it 'defaults to ENV[VCR_MODE] == LOCAL_MODE' do
+      expected = ENV['VCR_MODE'] == klass::LOCAL_MODE
+
+      configuration.local_mode.must_equal expected
     end
 
-    describe '#wipe_cassette_dir' do
-      it 'defaults to WIPE_CASSETTE_DIR' do
-        configuration.wipe_cassette_dir.must_equal klass::WIPE_CASSETTE_DIR
-      end
+    it 'returns assigned value' do
+      configuration.local_mode = true
 
-      it 'returns assigned value' do
-        configuration.wipe_cassette_dir = true
+      configuration.local_mode.must_equal true
+    end
+  end
 
-        configuration.wipe_cassette_dir.must_equal true
-      end
+  describe '#enable_deltas' do
+    it 'defaults to ENABLE_DELTAS' do
+      configuration.enable_deltas.must_equal klass::ENABLE_DELTAS
     end
 
-    describe '#local_mode' do
-      it 'defaults to ENV[VCR_MODE] == LOCAL_MODE' do
-        expected = ENV['VCR_MODE'] == klass::LOCAL_MODE
+    it 'returns assigned value' do
+      configuration.enable_deltas = true
 
-        configuration.local_mode.must_equal expected
-      end
+      configuration.enable_deltas.must_equal true
+    end
+  end
 
-      it 'returns assigned value' do
-        configuration.local_mode = true
+  describe '#deltas_enabled?' do
+    it 'is an alias for `enable_deltas`' do
+      configuration.deltas_enabled?.must_equal configuration.enable_deltas
 
-        configuration.local_mode.must_equal true
-      end
+      configuration.enable_deltas = true
+
+      configuration.deltas_enabled?.must_equal configuration.enable_deltas
+    end
+  end
+
+  describe '#deltas_disabled?' do
+    it 'is true when `deltas_enabled?` is false' do
+      configuration.deltas_enabled?.must_equal false
+      configuration.deltas_disabled?.must_equal true
     end
 
-    describe '#enable_deltas' do
-      it 'defaults to ENABLE_DELTAS' do
-        configuration.enable_deltas.must_equal klass::ENABLE_DELTAS
-      end
+    it 'is false when `deltas_enabled?` is true' do
+      configuration.enable_deltas = true
 
-      it 'returns assigned value' do
-        configuration.enable_deltas = true
+      configuration.deltas_enabled?.must_equal true
+      configuration.deltas_disabled?.must_equal false
+    end
+  end
 
-        configuration.enable_deltas.must_equal true
-      end
+  describe '#delta_directory' do
+    it 'defaults to DELTA_DIRECTORY' do
+      configuration.delta_directory.must_equal klass::DELTA_DIRECTORY
     end
 
-    describe '#deltas_enabled?' do
-      it 'is an alias for `enable_deltas`' do
-        configuration.deltas_enabled?.must_equal configuration.enable_deltas
+    it 'returns assigned value' do
+      configuration.delta_directory = 'somedir'
 
-        configuration.enable_deltas = true
+      configuration.delta_directory.must_equal 'somedir'
+    end
+  end
 
-        configuration.deltas_enabled?.must_equal configuration.enable_deltas
-      end
+  describe '#current_delta_name' do
+    it 'defaults to CURRENT_DELTA_NAME' do
+      configuration.current_delta_name.must_equal klass::CURRENT_DELTA_NAME
     end
 
-    describe '#deltas_disabled?' do
-      it 'is true when `deltas_enabled?` is false' do
-        configuration.deltas_enabled?.must_equal false
-        configuration.deltas_disabled?.must_equal true
-      end
+    it 'returns assigned value' do
+      configuration.current_delta_name = 'somefile'
 
-      it 'is false when `deltas_enabled?` is true' do
-        configuration.enable_deltas = true
-
-        configuration.deltas_enabled?.must_equal true
-        configuration.deltas_disabled?.must_equal false
-      end
-    end
-
-    describe '#delta_directory' do
-      it 'defaults to DELTA_DIRECTORY' do
-        configuration.delta_directory.must_equal klass::DELTA_DIRECTORY
-      end
-
-      it 'returns assigned value' do
-        configuration.delta_directory = 'somedir'
-
-        configuration.delta_directory.must_equal 'somedir'
-      end
-    end
-
-    describe '#current_delta_name' do
-      it 'defaults to CURRENT_DELTA_NAME' do
-        configuration.current_delta_name.must_equal klass::CURRENT_DELTA_NAME
-      end
-
-      it 'returns assigned value' do
-        configuration.current_delta_name = 'somefile'
-
-        configuration.current_delta_name.must_equal 'somefile'
-      end
+      configuration.current_delta_name.must_equal 'somefile'
     end
   end
 end
